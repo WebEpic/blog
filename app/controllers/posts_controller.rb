fdbf8613dbd491424ce005fe9ab640e97768f7b5
WebEpic <randy@randyjmitchell.com>
2015-04-21 04:03:18 -0400

    add validations to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,21 +4,25 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 	  @post = Post.new(post_params)
-	  
-	  @post.save
-	  redirect_to @post
-	end
 
-	def show
-		@post = Post.find(params[:id])
+	  if @post.save
+	  	redirect_to @post
+  	else
+  	  	render 'new'
 	end
+  end
 
-	private
-	  def post_params
-	  	params.require(:post).permit(:title, :body)
+  def show
+	@post = Post.find(params[:id])
+  end
+
+  private
+	def post_params
+	  params.require(:post).permit(:title, :body)
   	end
 end
