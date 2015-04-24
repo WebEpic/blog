b831af9fb6ce0f1b87f4a351c525f3f751d7d24a
WebEpic <randy@randyjmitchell.com>
2015-04-24 01:17:04 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
+  get '/about', to: 'pages#about'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
